c7299d352bb6f8fc15e53fa3154b74e6f3060714
BUGTeas <99161186+BUGTeas@users.noreply.github.com>
2023-01-19 16:00:55 +0800

    (20230119)Fixed the bug stuck in loading interface

--- recordTest.js
+++ recordTest.js
@@ -1,4 +1,5 @@
 //实验性功能：录屏
+var format;
 canv = document.getElementById("canv");
 const ctx = canv.getContext('2d');
 function playCanvas(ctx){
@@ -10,45 +11,47 @@ function playCanvas(ctx){
 }
 recStatus = false;
 function setRecorder(){
-    recAllChunks = [];
-    var formatlist = ["h264","vp9","vp8"];
-    for(i=0;i<formatlist.length;i++){
-        if(MediaRecorder.isTypeSupported('video/webm;codecs=' + formatlist[i])){
-            format = formatlist[i]
-            i = formatlist.length;
-            if(format!=formatlist[0]) sID("recordopt_tips").innerHTML = "当前浏览器不支持 " + formatlist[0] + " 编码，将使用 " + format + " 编码。";
-        }
-    }
-    console.log("录制器设置完成，编码为 " + format);
-    const stream = canv.captureStream(60); // 60 FPS recording
-    const recorder = new MediaRecorder(stream, {
-        mimeType: 'video/webm;codecs=' + format
-    });
-    recorder.ondataavailable = e => {
-        recAllChunks.push(
-            e.data
-        );
-    }
     recStart = document.createElement("button");
     recStop = document.createElement("button");
-    recStart.onclick = e => {
-        recorder.start(10);
+    recAllChunks = [];
+    var formatlist = ["h264","vp9","vp8","none"];
+    for(i=0;i<formatlist.length;i++){
+        format = formatlist[i];    
+        if(format!="none"&&MediaRecorder.isTypeSupported('video/webm;codecs=' + format)){
+            i = formatlist.length;
+            if(format!=formatlist[0]) sID("recordopt_tips").innerHTML = "当前浏览器不支持 " + formatlist[0] + " 编码，将使用 " + format + " 编码。";
+            const stream = canv.captureStream(30); //30帧
+            const recorder = new MediaRecorder(stream, {
+                mimeType: 'video/webm;codecs=' + format
+            });
+            recorder.ondataavailable = e => {
+                recAllChunks.push(
+                    e.data
+                );
+            }
+            recStart.onclick = e => {
+                recorder.start(10);
+            }
+            recStop.onclick = e => {
+                recorder.stop();
+                const link = document.createElement('a');
+                link.style.display = 'none';
+                const fullBlob = new Blob(recAllChunks);
+                const downloadUrl = window.URL.createObjectURL(fullBlob);
+                link.href = downloadUrl;
+                link.download = 'media.mp4';
+                document.body.appendChild(link);
+                link.click();
+                link.remove();
+                console.log("停止录制")
+                setRecorder();
+            }
+            console.log("录制器设置完成，编码为 " + format);
+        }
+        else{
+            sID("recordopt_tips").innerHTML = "当前浏览器不支持录制功能。";
+            console.log(sID("recordopt_tips").innerHTML);
+        }
     }
-    recStop.onclick = e => {
-        recorder.stop();
-        recBlobDownload();
-    }
-}
-function recBlobDownload() {
-    const link = document.createElement('a');
-    link.style.display = 'none';
-    const fullBlob = new Blob(recAllChunks);
-    const downloadUrl = window.URL.createObjectURL(fullBlob);
-    link.href = downloadUrl;
-    link.download = 'media.mp4';
-    document.body.appendChild(link);
-    link.click();
-    link.remove();
-    setRecorder();
 }
 recorderWork = true;
